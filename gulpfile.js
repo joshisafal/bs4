717e9a6e369358954c7d42340e498a757e5e6f53
joshisafal <joshisafalnp@gmail.com>
2019-07-09 18:50:32 +0545

    navbar is completed

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,6 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
-
 // Move the javascript files into our /src/js folder
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
@@ -18,16 +17,15 @@ gulp.task('js', function() {
         .pipe(browserSync.stream());
 });
 
-
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', gulp.series('sass', function() {
 
     browserSync.init({
         server: "./src"  
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], gulp.series('sass'));
     gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+}));
 
-gulp.task('default', ['js','serve']);
+gulp.task('default', gulp.parallel('js','serve'));
